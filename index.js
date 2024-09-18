a1888233c8811291c6a562bc44fda4fe911e15a6
rukhsarnaveed12 <mk8076750@gmail.com>
2024-09-18 08:00:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//QNO 1
+var name="Rukhsar"
+alert(name);
+
+//QNO 2
+var x="Rukhsar"
+alert("hello ,I have done my assignment work"+" "+x);
+console.log(x);
+
+//qno3
+var a="4";
+var b="2";
+var x=(a++ + --b - --a + b++ + a+b);
+console.log(x);
+
+//qno4
+var time = prompt("how much u sleeping time?")
+if (time > 1 && time < 10){
+    alert("good morning");
+}else if(time > 11 && time < 15){
+    alert("good afternoon");
+}else if(time > 15 && time < 18){
+    alert("good evening");
+}else {alert("good night");
+}
